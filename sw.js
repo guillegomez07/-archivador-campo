ce0c5fddde90c0949a3391cb9741d278866142a6
guillegomez07 <guillegomez07@hotmail.com>
2026-09-16 12:43:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'archivador-campo-v1';
+const CACHE_NAME = 'archivador-campo-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
